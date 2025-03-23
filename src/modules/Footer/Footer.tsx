3412641e25dd9264b3617fe8ca43db2fbe2daf28
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2025-03-23 22:04:04 +0300

    Add date-fns dependency and update Footer component for dynamic year display

--- src/modules/Footer/Footer.tsx
+++ src/modules/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import React, { JSX } from 'react';
 import styles from './Footer.module.scss';
 import cn from 'classnames';
 import { FooterProps } from './Footer.props';
+import { format } from 'date-fns'
 
 export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
@@ -10,9 +11,11 @@ export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 		}
 			{ ...props }
 		>
-			<div>
-				OwlTop © 2020 - 2021 Все права защищены
-			</div>
+			<span>
+				OwlTop © 2020 - { format(new Date(), 'yyyy') } Все права защищены
+			</span>
+			<a href="#" target="_blank">Пользовательское соглашение</a>
+			<a href="#" target="_blank">Политика конфиденциальности</a>
 		</div>
 	);
 };
