94e8056c6e6a680d975040627c68a5419603f3ee
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2025-03-25 13:48:54 +0300

    Add Home module with dynamic menu fetching and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,17 @@
-'use client'
+import { MenuItem } from '@/interfaces/menu.inteface';
+import Home from '@/modules/Home/Home';
+import axios from 'axios';
 
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Button, Title, P, Rating } from '@/components';
-import { Tag } from '../components/Tag/Tag';
-import { useEffect, useState } from 'react';
+export default async function Page() {
+  const firstCategory = 0;
 
-
-export default function Home() {
-
-	const [counter, setCounter] = useState<number>(0);
-	const [rating, setRating] = useState<number>(0);
-
-	useEffect(() => {
-		console.log('render', counter);
-
-	});
-
-	
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-				<Title tag="h1">Заголовок</Title>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-				<p>Привет счетчик { counter }</p>
-				<Button onClick={ () => setCounter(counter + 1) } arrow={ 'right' } >Кнопка</Button>
-				<Button onClick={ () => setCounter(counter - 1) } arrow={ 'down' } >Кнопка</Button>
-				<Button appearances={ 'ghost' }>Кнопка</Button>
-
-				<P size={ 'l' }>Текст l</P>
-				<P>Текст m</P>
-				<P size={ 's' }>Текст s</P>
-
-				<Tag size={ 's' } color={ 'red' }>Метка red</Tag>
-				<Tag size={ 'm' } color={ 'green' }>Метка green</Tag>
-				<Tag size={ 'm' } color={ 'primary' }>Метка primary</Tag>
-				<Tag size={ 'm' } href={ '#' } color={ 'grey' }>Метка grey</Tag>
-
-				<Rating rating={ rating } isEditable setRating={ setRating } />
-      </main>
-    </div>
+  // Запрос данных на сервере
+  const { data: menu } = await axios.post<MenuItem[]>(
+    `${process.env.NEXT_PUBLIC_DOMAIN}/api/top-page/find`, {
+			firstCategory
+		}
   );
+
+  return <Home menu={menu} firstCategory={firstCategory} />;
 }
+
---
+++ src/modules/Home/Home.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import Image from "next/image";
+import styles from "./Home.module.css";
+import { Button, Title, P, Rating, Tag } from '@/components';
+import { useEffect, useState } from 'react';
+import { MenuItem } from '@/interfaces/menu.inteface';
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
+
+
+export default function Home({ menu }: HomeProps) {
+
+	const [counter, setCounter] = useState<number>(0);
+	const [rating, setRating] = useState<number>(0);
+
+	useEffect(() => {
+		console.log('render');
+
+	} , []);
+
+
+	return (
+		<div className={ styles.page }>
+			<main className={ styles.main }>
+				<Title tag="h1">Заголовок</Title>
+				<Image
+					className={ styles.logo }
+					src="/next.svg"
+					alt="Next.js logo"
+					width={ 180 }
+					height={ 38 }
+					priority
+				/>
+				<p>Привет счетчик { counter }</p>
+				<Button onClick={ () => setCounter(counter + 1) } arrow={ 'right' } >Кнопка</Button>
+				<Button onClick={ () => setCounter(counter - 1) } arrow={ 'down' } >Кнопка</Button>
+				<Button appearances={ 'ghost' }>Кнопка</Button>
+
+				<P size={ 'l' }>Текст l</P>
+				<P>Текст m</P>
+				<P size={ 's' }>Текст s</P>
+
+				<Tag size={ 's' } color={ 'red' }>Метка red</Tag>
+				<Tag size={ 'm' } color={ 'green' }>Метка green</Tag>
+				<Tag size={ 'm' } color={ 'primary' }>Метка primary</Tag>
+				<Tag size={ 'm' } href={ '#' } color={ 'grey' }>Метка grey</Tag>
+
+				<Rating rating={ rating } isEditable setRating={ setRating } />
+				{ menu.map(m => (
+					<div key={ m._id.secondCategory }>
+						{ m._id.secondCategory }
+						<hr />
+					</div>
+				)) }
+			</main>
+		</div>
+	);
+}
+
